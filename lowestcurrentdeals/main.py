863cec0e058dc1966061df2571665f83d0d42485
gEvent <grace.egbo1@gmail.com>
2015-07-28 11:57:09 -0400

    I added the APIs fo Walmart and Bestbuy and for the results.html file I simply iterated over the lists using jinja

--- lowestcurrentdeals/main.py
+++ lowestcurrentdeals/main.py
@@ -23,6 +23,7 @@ import os
 import jinja2
 import json
 import urllib2
+from google.appengine.api import urlfetch
 
 jinja_environment=jinja2.Environment(
     loader=jinja2.FileSystemLoader(
@@ -40,12 +41,27 @@ class SearchHandler(webapp2.RequestHandler):
 # via their respective APIs
 
 class ResultHandler(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
+        # Gets data source for Bestbuy
+        bestbuy_data_source = urlfetch.fetch("http://api.remix.bestbuy.com/v1/products(name=%s*)?format=json&show=name,salePrice,image&sort=salePrice.desc&apiKey=24ta6vtsr78a22fmv8ngfjet" %self.request.get('query').replace(' ','+'))
+        bestbuy_json_content = bestbuy_data_source.content
+        parsed_bestbuy_dictionary = json.loads(bestbuy_json_content)
+
+        # Adds the atributes name, image pic, and price to a bestbuy list
+        bestbuy_product=['<img src =%s>' %parsed_bestbuy_dictionary['products'][0]['image'],parsed_bestbuy_dictionary['products'][0]['name'],parsed_bestbuy_dictionary['products'][0]['salePrice']]
+
+        # Gets data source for Walmart
+
+        walmart_data_source = urlfetch.fetch("http://api.walmartlabs.com/v1/search?query=%s&format=json&apiKey=cz9kfm3vuhssnk6hn33zg86k" %self.request.get('query').replace(' ','+'))
+        walmart_json_content = walmart_data_source.content
+        parsed_walmart_dictionary = json.loads(walmart_json_content)
+
+        # Adds the atributes name, image pic, and price to a walmart list
+        walmart_product=['<img src =%s>' %parsed_walmart_dictionary['items'][0]['thumbnailImage'],parsed_walmart_dictionary['items'][0]['name'],parsed_walmart_dictionary['items'][0]['salePrice']]
+
         template=jinja_environment.get_template('/templates/results.html')
-        template_variables={"user_search":self.request.get('search')}
+        template_variables={"user_search":self.request.get('search'),'bestbuy':bestbuy_product,'walmart':walmart_product}
         self.response.write(template.render(template_variables))
-        # returns in JSON name, salePrice, and URL of user's search from BestBuy
-        self.response.write(self.response.out.write(urllib2.urlopen('http://api.remix.bestbuy.com/v1/products(search='+template_variables["user_search"]+')?format=json&show=sku,name,salePrice,url&apiKey=24ta6vtsr78a22fmv8ngfjet').read()))
 
 class WishListHandler(webapp2.RequestHandler):
     def get(self):
