51b780c1555d212526f2a7803ac2a738a2add6b3
gEvent <grace.egbo1@gmail.com>
2015-07-29 11:03:15 -0400

    Modified the wishlist handler to create identification between user and add to datastore and in results.html I created an extra line to display Add to Wishlist

--- lowestcurrentdeals/main.py
+++ lowestcurrentdeals/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+   #!/usr/bin/env python
 #
 # Copyright 2007 Google Inc.
 #
@@ -27,10 +27,20 @@ from test_settings import *
 from amazon.api import AmazonAPI
 from google.appengine.api import urlfetch
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 jinja_environment=jinja2.Environment(
     loader=jinja2.FileSystemLoader(
         os.path.dirname(__file__)))
+
+class WishList(ndb.Model):
+    user_id = ndb.StringProperty()
+    store = ndb.StringProperty()
+    item_id = ndb.StringProperty()
+    name = ndb.StringProperty()
+    price = ndb.StringProperty()
+    image = ndb.StringProperty()
+    url = ndb.StringProperty()
 
 # handles input for searches
 class SearchHandler(webapp2.RequestHandler):
@@ -57,27 +67,71 @@ class ResultHandler(webapp2.RequestHandler):
         search = self.request.get('search')
         amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
         amazon_results = amazon.search_n(15, Keywords=search, SearchIndex='All')
-        template=jinja_environment.get_template('/templates/results.html')
+
         # returns in JSON name, salePrice, and URL of user's search from BestBuy
         best_buy_url = 'http://api.remix.bestbuy.com/v1/products(search='+ search.replace(' ', '&search=')+')?format=json&show=sku,name,salePrice,url,image&pageSize=15&page=5&apiKey=24ta6vtsr78a22fmv8ngfjet'
         best_buy_results = json.load(urllib2.urlopen(best_buy_url)).get('products')
+
         walmart_url = "http://api.walmartlabs.com/v1/search?query=%s&format=json&apiKey=cz9kfm3vuhssnk6hn33zg86k&responseGroup=base" % search.replace(' ','+')
         walmart_results = json.load(urllib2.urlopen(walmart_url)).get('items')
+
         results = []
         for product in amazon_results:
-            results += [(product.title, product.price_and_currency[0], product.offer_url, product.large_image_url, 'Amazon')]
+            results += [(product.title, product.price_and_currency[0], product.offer_url, product.large_image_url, 'Amazon','placeholder')]
+            #How to retrive asin for amazon products and decrease image size
         for product in best_buy_results:
-            results += [(product.get('name'), product.get('salePrice'), product.get('url'), product.get('image'), 'Best Buy')]
+            results += [(product.get('name'), product.get('salePrice'), product.get('url'), product.get('image'), 'Best Buy','/wishlist?type=bestbuy&id=%s'%product.get('sku'))]
         for product in walmart_results:
-            results += [(product.get('name'), product.get('salePrice'), product.get('productUrl'), product.get('thumbnailImage'), 'Walmart')]
+            results += [(product.get('name'), product.get('salePrice'), product.get('productUrl'), product.get('thumbnailImage'), 'Walmart','/wishlist?type=walmart&id=%s'%product.get('itemId'))]
         results = sorted(results,key=lambda x: x[1])
         template_variables={"user_search":search, 'results':results}
+
+        template=jinja_environment.get_template('/templates/results.html')
         self.response.write(template.render(template_variables))
 
 
 
 class WishListHandler(webapp2.RequestHandler):
     def get(self):
+
+        user = users.get_current_user()
+        person_id = user.user_id()
+
+        store_type = self.request.get('type')
+        product_id = self.request.get('id')
+
+        if store_type.lower() == "walmart":
+            walmart_url=("http://api.walmartlabs.com/v1/items/%s?format=json&apiKey=cz9kfm3vuhssnk6hn33zg86k" %product_id)
+            walmart_JSON_string=json.load(urllib2.urlopen(walmart_url))
+
+            walmart_image_source="<img src=%s>" %walmart_JSON_string["thumbnailImage"]
+
+            walmart_name=walmart_JSON_string["name"]
+
+            sales_Price = str(walmart_JSON_string["salePrice"])
+
+            walmart_link_to_buy = walmart_JSON_string["productUrl"]
+
+            walmart = WishList(user_id = person_id, store = store_type, item_id = product_id,name = walmart_name, price = sales_Price, image = walmart_image_source, url = walmart_link_to_buy)
+
+            walmart.put()
+
+        if store_type.lower() == "bestbuy":
+            bestbuy_url=("http://api.remix.bestbuy.com/v1/products/%s.json?show=sku,name,salePrice,url,image&apiKey=24ta6vtsr78a22fmv8ngfjet" %product_id)
+            bestbuy_JSON_string=json.load(urllib2.urlopen(bestbuy_url))
+
+            bestbuy_image_source="<img src=%s>" %bestbuy_JSON_string["image"]
+
+            bestbuy_name=bestbuy_JSON_string["name"]
+
+            bestbuy_Price = str(bestbuy_JSON_string["salePrice"])
+
+            bestbuy_link_to_buy = bestbuy_JSON_string["url"]
+
+            bestbuy = WishList(user_id = person_id, store = store_type, item_id = product_id,name = bestbuy_name, price = bestbuy_Price, image = bestbuy_image_source, url = bestbuy_link_to_buy)
+
+            bestbuy.put()
+
         template=jinja_environment.get_template('/templates/wishlist.html')
         self.response.write(template.render())
 
